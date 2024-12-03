5edb275d409513776bf491440482a76ac998856b
Jonathan Sigrist <jonathan.sigrist@xitaso.com>
2024-12-03 11:13:20 +0100

    calculation method tests

--- src/app/[locale]/viewer/_components/submodel/carbon-footprint/visualization-components/CalculationMethod.tsx
+++ src/app/[locale]/viewer/_components/submodel/carbon-footprint/visualization-components/CalculationMethod.tsx
@@ -1,16 +1,16 @@
 import { Link, Typography } from '@mui/material';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 
-const LinkGHG = 'https://ghgprotocol.org/';
+export const LinkGHG = 'https://ghgprotocol.org/';
 
 export function CalculationMethod(props: { calculationMethod: string }) {
     const { calculationMethod } = props;
 
     return (
-        <Typography sx={{ color: 'primary.main', fontSize: 24, fontWeight: 'bold' }}>
+        <Typography sx={{ color: 'primary.main', fontSize: 24, fontWeight: 'bold' }} data-testid="calculation-method">
             {calculationMethod}{' '}
             {calculationMethod === 'GHG Protocol' && (
-                <Link href={LinkGHG} target="_blank">
+                <Link href={LinkGHG} target="_blank" data-testid="calculation-method-link">
                     <OpenInNewIcon fontSize="small" />
                 </Link>
             )}
