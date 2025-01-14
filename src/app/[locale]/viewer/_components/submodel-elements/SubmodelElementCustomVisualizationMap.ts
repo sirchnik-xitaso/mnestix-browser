1aa70ee6bd0cd6007e9735789c3968007b7f22b6
Alina Geesen <alina.geesen@xitaso.com>
2025-01-14 10:34:28 +0100

    docs(custom-submodel-visualization):add docstrings to mappings
    * docs(custom-submodel-visualization):add docstrings to mappings
    
    * docs(custom-submodel-visualization):fix broken link
    
    ---------
    
    Co-authored-by: Jonathan Sigrist <jonathan.sigrist@xitaso.com>

--- src/app/[locale]/viewer/_components/submodel-elements/SubmodelElementCustomVisualizationMap.ts
+++ src/app/[locale]/viewer/_components/submodel-elements/SubmodelElementCustomVisualizationMap.ts
@@ -4,6 +4,12 @@ import { MarkingsComponent } from './marking-components/MarkingsComponent';
 import { AddressComponent } from './address-component/AddressComponent';
 import { DocumentComponent } from './document-component/DocumentComponent';
 
+/**
+ * This represents the mapping between the submodel elements and the submodel element components to be shown.
+ * If you want to create a new custom submodel visualization, add the respectve submodel elements here.
+ * A detailed description on how to create custom submodel visualizations can be found here:
+ * https://github.com/eclipse-mnestix/mnestix-browser/wiki/How-to-create-custom-submodel-visualizations
+ */
 export const submodelElementCustomVisualizationMap = {
     [SubmodelElementSemanticId.Address]: AddressComponent,
     [SubmodelElementSemanticId.ContactInformation]: ContactInformationComponent,
