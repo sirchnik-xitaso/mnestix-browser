db4d9b93357a45433958fee9699a10f119acf635
Niklas Sirch-XITASO <niklas.sirch@xitaso.com>
2025-03-05 08:40:26 +0100

    chore: update next-intl

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -4,12 +4,12 @@ import type { Metadata } from 'next';
 import { ClientLayout } from 'app/[locale]/clientLayout';
 import { getMessages } from 'next-intl/server';
 import { NextIntlClientProvider } from 'next-intl';
+import { routing } from 'i18n/routing';
+import { notFound } from 'next/navigation';
 
 export type LocalizedIndexLayoutProps = {
     children: ReactNode;
-    params: {
-        locale: 'en' | 'de';
-    };
+    params: Promise<{ locale: string }>;
 };
 
 export const metadata: Metadata = {
@@ -19,9 +19,14 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({ children, params }: Readonly<LocalizedIndexLayoutProps>) {
     const messages = await getMessages();
+    const { locale } = await params;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    if (!routing.locales.includes(locale as any)) {
+        notFound();
+    }
 
     return (
-        <html lang={params.locale}>
+        <html lang={locale}>
             <body>
                 <AppRouterCacheProvider>
                     <NextIntlClientProvider messages={messages}>
