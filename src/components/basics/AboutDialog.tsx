1b5d714716a2432391903f831f9fe6e5ad74887b
Jonathan Sigrist <jonathan.sigrist@xitaso.com>
2025-03-13 10:19:03 +0100

    chore(footer): Fix styling of Footer (#126)
    * Dont show logo on mobile
    * Remove nested Typography
    * Replace NavigationBar by a simple dynamic Grid
    * small adjustments for the styling

--- src/components/basics/AboutDialog.tsx
+++ src/components/basics/AboutDialog.tsx
@@ -2,6 +2,7 @@
 import CloseIcon from '@mui/icons-material/Close';
 import { useTranslations } from 'next-intl';
 import { MnestixLogo } from './MnestixLogo';
+import { useIsMobile } from 'lib/hooks/UseBreakpoints';
 
 type AboutDialogProps = {
     readonly onClose: () => void;
@@ -11,6 +12,7 @@ type AboutDialogProps = {
 export function AboutDialog(props: AboutDialogProps) {
     const t = useTranslations('footer');
     const theme = useTheme();
+    const isMobile = useIsMobile();
 
     return (
         <Dialog open={props.open} onClose={props.onClose} maxWidth="md" fullWidth={true}>
@@ -29,40 +31,47 @@ export function AboutDialog(props: AboutDialogProps) {
             <DialogContent style={{ padding: '40px' }}>
                 <Box display="flex" flexDirection="column" gap="20px">
                     <Box display="flex" flexDirection="row" justifyContent="space-between">
-                        <Typography variant="h2" color={'primary'}>
-                            {t('about')}
-                            <Typography>MIT License</Typography>
-                            <Typography>Copyright (c) 2024 XITASO GmbH</Typography>
-                        </Typography>
-                        <Box
-                            bgcolor={theme.palette.primary.main}
-                            borderRadius="3%"
-                            justifyContent={'center'}
-                            alignItems={'center'}
-                            display={'flex'}
-                            p={2}>
-                            <MnestixLogo width={'230px'}/>
+                        <Box>
+                            <Typography variant="h2" color={'primary'}>
+                                {t('about')}
+                            </Typography>
+                            <Typography color={'primary'}>MIT License</Typography>
+                            <Typography color={'primary'}>Copyright (c) 2024 XITASO GmbH</Typography>
                         </Box>
+                        {!isMobile && (
+                            <Box
+                                bgcolor={theme.palette.primary.main}
+                                borderRadius={'0.5rem'}
+                                justifyContent={'center'}
+                                alignItems={'center'}
+                                display={'flex'}
+                                p={'1rem'}
+                                m={'0.5rem'}
+                                mr={'1rem'}
+                            >
+                                <MnestixLogo width={'15rem'} />
+                            </Box>
+                        )}
                     </Box>
                     <Box>
                         <Typography color="text.secondary">
                             Permission is hereby granted, free of charge, to any person obtaining a copy of this
-                            software and associated documentation files (the &quot;Software&quot;), to deal in the Software
-                            without restriction, including without limitation the rights to use, copy, modify, merge,
-                            publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
-                            to whom the Software is furnished to do so, subject to the following conditions:
+                            software and associated documentation files (the &quot;Software&quot;), to deal in the
+                            Software without restriction, including without limitation the rights to use, copy, modify,
+                            merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+                            persons to whom the Software is furnished to do so, subject to the following conditions:
                             <br />
                             <br />
                             The above copyright notice and this permission notice shall be included in all copies or
                             substantial portions of the Software.
                             <br />
                             <br />
-                                THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF
-                            ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-                            MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-                            AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
-                            AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-                            SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+                            THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+                            IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+                            PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+                            BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+                            OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+                            DEALINGS IN THE SOFTWARE.
                         </Typography>
                     </Box>
                 </Box>
--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,4 +1,5 @@
-import { BottomNavigation, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+import Grid from '@mui/material/Grid2';
 import Link from '@mui/material/Link';
 import { useEnv } from 'app/env/provider';
 import { useTranslations } from 'next-intl';
@@ -19,73 +20,71 @@ export function Footer() {
 
     return (
         <>
-            <BottomNavigation sx={{ bottom: 0, left: 0, right: 0, backgroundColor: 'transparent' }}>
-                <Typography
-                    color="text.secondary"
-                    fontSize="small"
-                    mr={2}
-                    display='flex'
-                    alignItems='center'
-                    justifyContent='center'>
-                    {copyrightString}
-                </Typography>
-
-                {dataPrivacyString && (
-                    <Typography
-                        fontSize="small" display='flex' maxWidth='150px'
-                        alignItems={'center'}
+            <Box
+                sx={{ my: 2, backgroundColor: 'transparent' }}
+                flexGrow={1}
+                display={'flex'}
+                justifyContent={'center'}
+                alignItems={'center'}
+                flexDirection={'row'}
+            >
+                <Grid container spacing={2}>
+                    <Grid
+                        size={{ xs: 12, md: 'auto' }}
+                        display={'flex'}
                         justifyContent={'center'}
+                        alignItems={'center'}
                     >
-                        <Link href={dataPrivacyString} target="_blank">
-                            <Typography>{t('dataPrivacy')}</Typography>
-                        </Link>
-                    </Typography>
-                )}
+                        <Typography color="text.secondary" fontSize="small">
+                            {copyrightString}
+                        </Typography>
+                    </Grid>
 
-                {dataPrivacyString && imprintString && (
-                    <Typography margin={2}
-                        color="text.secondary"
-                        fontSize="small"
-                        display='flex'
-                        alignItems='center'
-                        justifyContent='center'
+                    <Grid
+                        size={{ xs: 12, md: 'auto' }}
+                        flexDirection={'row'}
+                        display={'flex'}
+                        justifyContent={'center'}
+                        alignItems={'center'}
                     >
-                        |
-                    </Typography>
-                )}
+                        {dataPrivacyString && (
+                            <Typography fontSize="small" maxWidth="10rem">
+                                <Link href={dataPrivacyString} target="_blank">
+                                    {t('dataPrivacy')}
+                                </Link>
+                            </Typography>
+                        )}
 
-                {imprintString && (
-                    <Typography
-                        fontSize="small" maxWidth='150px' display='flex' alignItems='center' justifyContent='center'>
-                        <Link href={imprintString} target="_blank">
-                            <Typography>{t('imprint')}</Typography>
-                        </Link>
-                    </Typography>
-                )}
+                        {dataPrivacyString && (
+                            <Typography mx={2} color="text.secondary" fontSize="small">
+                                |
+                            </Typography>
+                        )}
 
-                { imprintString && (
-                    <Typography margin={2}
-                                color="text.secondary"
-                                fontSize="small"
-                                display='flex'
-                                alignItems='center'
-                                justifyContent='center'>
-                        |
-                    </Typography>
-                )}
+                        {imprintString && (
+                            <Typography fontSize="small" maxWidth="10rem">
+                                <Link href={imprintString} target="_blank">
+                                    {t('imprint')}
+                                </Link>
+                            </Typography>
+                        )}
+                        
+                        {imprintString && (
+                            <Typography mx={2} color="text.secondary" fontSize="small">
+                                |
+                            </Typography>
+                        )}
 
-                <Typography
-                    fontSize="small"
-                    display='flex'
-                    alignItems='center'
-                    justifyContent='center'
-                    maxWidth='250px'
-                >
-                    <Link href='#'>
-                        <Typography onClick={() => setAboutDialogOpen(!aboutDialogOpen)}>{t('about')}</Typography>
-                    </Link>
-                </Typography>
-            </BottomNavigation>
+                        <Typography
+                            fontSize="small"
+                            maxWidth="10rem"
+                            onClick={() => setAboutDialogOpen(!aboutDialogOpen)}
+                        >
+                            <Link href="#">{t('about')}</Link>
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Box>
             <AboutDialog open={aboutDialogOpen} onClose={handleAboutDialogClose}></AboutDialog>
         </>
     );
