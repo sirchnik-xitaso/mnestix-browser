eb6b74008c2b5a0c7a14ee123776aad27dcd005c
Niklas Sirch-XITASO <niklas.sirch@xitaso.com>
2025-04-04 10:21:16 +0200

    chore: use semantic-release for release process

---
+++ .releaserc.js
@@ -0,0 +1,36 @@
+const imageName = 'mnestix/mnestix-browser';
+
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+const config = {
+    tagFormat: 'release/mnestix-browser-v${version}',
+    branches: ['main', { name: 'next', prerelease: true }],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        [
+            '@semantic-release/exec',
+            {
+                publishCmd: `\
+            docker buildx build \
+              --platform linux/amd64,linux/arm64 \
+              --target=production \
+              --cache-from=type=local,src=/tmp/buildx-amd64-cache \
+              --cache-from=type=local,src=/tmp/buildx-arm64-cache \
+              -t ${imageName}:\${nextRelease.version} \
+              -t ${imageName}:latest \
+              --push . \
+              `,
+            },
+        ],
+        [
+            '@semantic-release/github',
+            {
+                releaseNameTemplate: '<%= nextRelease.version %>',
+            },
+        ],
+    ],
+};
+
+export default config;
